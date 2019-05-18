4dea38579f65f15e52a3f1d2e9cb73410a37c449
mccoymil <46498056+mccoymil@users.noreply.github.com>
2019-05-18 09:48:23 -0600

    Update dataBase.js

--- dataBase.js
+++ dataBase.js
@@ -9,7 +9,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_mccoymil',
-  password        : '5817',
+  password        : '',
   database        : 'cs340_mccoymil'
 });
 
